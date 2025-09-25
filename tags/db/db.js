0382306d8aa740d074cabe9ad01345205711f628
Prallav Aggarwal <prallavagg002@gmail.com>
2025-09-25 06:24:10 +0530

    trying to solve tag problem when adding blog

---
+++ tags/db/db.js
@@ -0,0 +1,54 @@
+//in this Tag Schema will be defined 
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+require('dotenv').config();
+mongoose.connect(process.env.DB_URL)
+
+
+//blog schema 
+const blog = new Schema({
+  title: String,
+  content: String,
+  imageUrl: String,
+  tags: [{ type: String, unique: true }],
+  createdAt: Date,
+  author: {
+    type: ObjectId,
+    ref: 'user'
+  },
+  clapCount: [{ type: ObjectId, ref: 'user' }],
+  responses: [{ type: ObjectId, ref: 'user' }]
+})
+
+const user = new Schema({
+  email: String,
+  username: String,
+  password: String,
+  profilePicture: String,
+  pronouns: String,
+  bio: String,
+  collection: [{ type: ObjectId, ref: 'blog' }],
+  favourites: [{ type: ObjectId, ref: 'blog' }],
+  blogs: [{ type: ObjectId, ref: 'blog' }],
+  tags: [{ type: ObjectId, ref: 'tag' }],
+  liked: [{ type: ObjectId, ref: 'blog' }],
+})
+
+
+const tagSchema = new Schema({
+  name: String,
+  blog: [{ type: ObjectId, ref: 'blog' }],
+  user: [{ type: ObjectId, ref: 'user' }]
+})
+
+const blogModel = mongoose.model('blog', blog);
+const userModel = mongoose.model('user', user);
+const tagModel = mongoose.model('tag', tagSchema);
+
+module.exports = {
+  blogModel,
+  userModel,
+  tagModel
+}
---
+++ tags/index.js
@@ -0,0 +1,159 @@
+//in this I am going to create tag system 
+//I am very much confused in this tag 
+
+
+const express = require('express');
+const app = express();
+const { userModel, tagModel, blogModel } = require('./db/db.js')
+
+app.use(express.json());
+
+app.get('/tag', (req, res) => {
+
+});
+
+app.post('/user/create', async (req, res) => {
+  try {
+
+    //gather data from req.body
+    let email = req.body.email;
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let user = await userModel.findOne({ email: email });
+    //if user already exist then return 
+    if (user) {
+      console.log("user with this email already floating in our systems.");
+      return res.status(400).json({
+        success: false,
+        message: "user with this email already floating in our systems.",
+      })
+    }
+
+    user = await userModel.create({ email: email, username: username, password: password });
+    console.log("entry successfully created in database\n", user);
+    return res.status(200).json({
+      success: true,
+      message: "Sign-up success",
+      user: user
+    })
+
+
+
+  } catch (error) {
+    console.log("Some error occured while sign-up.\n", error);
+    return res.status(500).json({
+      success: false,
+      message: "Some error occured while sign-up."
+    })
+
+  }
+})
+
+app.post('/blog/create', async (req, res) => {
+  try {
+
+    let title = req.body.title;
+    let content = req.body.content;
+    let imageUrl = req.body.imageUrl;
+    let date = Date().toLocaleString();
+    let userId = req.body.userId;
+    let tag = req.body.tag;
+
+    //pass array of tags.
+    console.log("tags : ", tag)
+    let tagEntry = await tagModel.find();
+    let isTagPresent = false;
+    let isUser = false;
+    console.log('tagEntry : ', tagEntry)
+    // tagEntry.forEach(obj => {
+    //   if (tag.includes(obj.name)) {
+    //     isTagPresent = true;
+    //     console.log('tag present ', obj.name)
+    //     if (obj.user.includes(userId)) {
+    //       isUser = true;
+    //       console.log('user present')
+    //     }
+    //     else {
+    //       obj.user.push(userId);
+    //     }
+    //   }else{
+    //     newTag.name = 
+    //   }
+    //
+    // })
+
+    console.log(typeof tag)
+    console.log(typeof JSON.stringify(tag))
+    let a = new Array();
+
+
+    let blogEntry = await blogModel.create({
+      title,
+      content,
+      imageUrl,
+      createdAt: date,
+      author: userId,
+      tags: tag
+    })
+
+
+    tag.forEach(async element => {
+      tagEntry.forEach(async obj => {
+        if (element == obj.name) {
+          //if tag name already in collection 
+          isTagPresent = true;
+          console.log('tag present ', obj.name)
+          if (obj.user.includes(userId)) {
+            isUser = true;
+            console.log('user present')
+          } else {
+            //if tag name not present in collection 
+            obj.user.push(userId);
+          }
+          obj.blog.push(blogEntry._id);
+          await obj.save();
+        }
+      })
+      //tagEntry ke loop se bahar
+      if (!isTagPresent) {
+        //if tag name not present in collection 
+        let newTag = new tagModel({
+          name: element,
+          user: [userId],
+          blog: [blogEntry._id]
+        })
+        await newTag.save();
+        // newTag.name = element;
+        // newTag.user.push(userId);
+      }
+    });
+
+
+    //if tag is not present then create new entry 
+    //add : name, blogId, userId
+
+    let userEntry = await userModel.findOneAndUpdate({ _id: userId }, {
+      $addToSet: { tags: { $each: tag } },
+      $push: { blogs: blogEntry._id }
+    }, { new: true })
+
+    console.log("user entry : ", userEntry)
+    console.log("tagentry after push", tagEntry)
+
+    return res.status(200).json({
+      message: "success",
+      tagEntry: tagEntry
+    })
+
+  } catch (error) {
+    console.log("Some error occured while creating blog.\n", error);
+    return res.status(500).json({
+      success: false,
+      message: "Some error occured while creating blog."
+    })
+
+  }
+})
+
+app.listen(3000);
